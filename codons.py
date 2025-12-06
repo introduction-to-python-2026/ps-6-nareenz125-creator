3a9b913e2a180a263c19d2fd1201311807a9ef99
nareenz125-creator <nareenz125@gmail.com>
2025-12-06 11:29:07 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,13 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    codon_dict = {}
+    with open(file_path) as file:
+        rows = file.readlines()
+    for row_str in rows:
+        parts = row_str.strip().split('\t')
+        if len(parts) >= 3:
+            codon = parts[0]
+            amino_acid_abbr = parts[2]
+            codon_dict[codon] = amino_acid_abbr
+    return codon_dict
 
 
